8d0b85f4c8032e7bdaa7412d311a02259d1222f4
Peter Neumark <neumark.peter@gmail.com>
2022-05-17 21:17:08 +0200

    added internal link navigation unit test

---
+++ packages/plugin-mdx/browser-tests/tiddlers/test-internal-links.js
@@ -0,0 +1,45 @@
+/*\
+title: test-internal-links.js
+type: application/javascript
+tags: [[$:/tags/test-spec]]
+
+\*/
+(function () {
+
+    /*jslint node: true, browser: true */
+    /*global $tw: false */
+    "use strict";
+
+    // these tests are browser-only
+    if (!$tw.browser) {
+        return
+    }
+
+    const { openTiddler, sleep, toJSON } = require('$:/plugins/tiddlybase/browser-test-utils/test-utils.js');
+
+    const getLinkElement = (tiddler, target) => document.querySelector(`div[data-tiddler-title="${tiddler}"] a[href="#${target}"]`)
+
+    const getOpenTiddlers = () => $tw.wiki.getTiddler('$:/HistoryList').fields['current-tiddler'];
+
+    describe("Navigation with internal links", function () {
+
+        it("Clicking on internal links navigates as expected", async function () {
+            const target = "InternalLinkTarget";
+            const source = "InternalLinkSource";
+            $tw.wiki.addTiddlers([
+                { title: source, text: `[[${target}]]`, type: "text/x-markdown"},
+                { title: target, text: `- foo\n- bar`, type: "text/x-markdown"}
+            ]);
+            await openTiddler(source);
+            const link = getLinkElement(source, target);
+            expect(link).toBeTruthy();
+            console.log('asdf');
+            link.click();
+            await sleep(0);
+            expect(getOpenTiddlers()).toEqual(target);
+        });
+
+    });
+
+
+})();
