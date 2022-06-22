fc4cecf213fec1f21efec22177e52d09268acbf5
Peter Neumark <neumark.peter@gmail.com>
2022-06-22 18:24:32 +0200

    fix tiddlydesktop bug

--- packages/plugin-tiddlybase-utils/src/navigator.ts
+++ packages/plugin-tiddlybase-utils/src/navigator.ts
@@ -2,17 +2,19 @@ import type {} from "@tiddlybase/tw5-types/src/index"
 
 export const findNavigator = (parent = $tw.rootWidget):$tw.Widget|undefined => {
   const isNavigator = (child:$tw.Widget) => child?.parseTreeNode?.type === 'navigator'
-  for (let child of parent.children || []) {
-      if (isNavigator(child)) {
-          // console.log("found navigator as child", child);
-          return child;
-      }
-      // console.log("searching for navigator in children");
-      const descendent = findNavigator(child);
-      if (descendent) {
-          return descendent;
-      }
+  if (parent) {
+    for (let child of parent.children || []) {
+        if (isNavigator(child)) {
+            // console.log("found navigator as child", child);
+            return child;
+        }
+        // console.log("searching for navigator in children");
+        const descendent = findNavigator(child);
+        if (descendent) {
+            return descendent;
+        }
 
+    }
   }
   return undefined;
 };
