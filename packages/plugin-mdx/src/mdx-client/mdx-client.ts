698443e7082859b261454f1b829f65917dc74f48
Peter Neumark <neumark.peter@gmail.com>
2022-07-16 19:38:47 +0200

    improve MDX warning + error reporting

--- packages/plugin-mdx/src/mdx-client/mdx-client.ts
+++ packages/plugin-mdx/src/mdx-client/mdx-client.ts
@@ -4,7 +4,8 @@ import { compile as compileMDX } from '@mdx-js/mdx'
 import remarkGfm from 'remark-gfm' // Tables, footnotes, strikethrough, task lists, literal URLs.
 import wikiLinkPlugin from 'remark-wiki-link';
 import * as ReactJSXRuntime from 'react/jsx-runtime';
-
+import remarkPresetLintConsistent from 'remark-preset-lint-consistent'
+import remarkPresetLintRecommended from 'remark-preset-lint-recommended'
 
 // replace async import expression with call to sync importFn()
 const fixImports = (body: string) => body.replace(/= await import\(/mg, "= await importFn(");
@@ -26,6 +27,8 @@ export const compile = async (name: string, mdx: string, contextKeys: string[] =
     // trimStart() is needed because mdx doesn't tolerate leading newlines
     const compilerOutput = await compileMDX(mdx.trimStart(), {
       remarkPlugins: [
+        remarkPresetLintConsistent,
+        remarkPresetLintRecommended,
         remarkGfm,
         [wikiLinkPlugin, {
           pageResolver: (x: string) => [x],
@@ -39,9 +42,11 @@ export const compile = async (name: string, mdx: string, contextKeys: string[] =
       jsxRuntime: 'automatic',
       jsxImportSource: 'react'
     });
-    const jsSource = wrap(name, String(compilerOutput.value), contextKeys)
+    const jsSource = wrap(name, String(compilerOutput.value), contextKeys);
+    console.log("compilerOutput", compilerOutput);
     return eval(jsSource);
   } catch (e) {
+    console.log("compiler error", Object.assign({}, e));
     throw new Error(`${Object.entries((e as any)?.position?.start ?? {}).map(([k, v]) => `${k}: ${String(v)}`).join(" ")} ${(e as Error).message}`);
   }
 }
