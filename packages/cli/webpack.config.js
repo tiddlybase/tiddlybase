b13b379301fc366563163539f3fadd3d5076b135
Peter Neumark <neumark.peter@gmail.com>
2022-07-28 10:39:32 +0200

    transpile cli code with webpack

---
+++ packages/cli/webpack.config.js
@@ -0,0 +1,8 @@
+const { getNodeConfig } = require('@tiddlybase/webpack-config');
+const path = require('path');
+
+module.exports = () => getNodeConfig({
+  input: path.resolve(__dirname, 'src/cli.ts'),
+  outputDir: path.join(__dirname, "dist"),
+  outputFilename: "tiddlybase.js"
+});
