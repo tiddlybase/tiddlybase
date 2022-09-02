2eed8e237aead827a87c61a211c903c52dad6c9f
Peter Neumark <neumark.peter@gmail.com>
2022-09-02 11:24:41 +0200

    fix wiki builder script, add minimal build

--- packages/wiki-builder/build-one.sh
+++ packages/wiki-builder/build-one.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
 dirname=$(echo "$1" | cut -d / -f 2-)
-yarn workspace @tiddlybase/cli run cli buildwiki -t html -p ./../../dist/plugins -p ./plugins -o ./../../dist/tiddlybase_public/$dirname.html ./$dirname
+# yarn workspace run changes the current directory, so $pwd needs to be saved beforehand and all paths must be absolute in command line arguments
+pwd="$(pwd)"
+yarn workspace @tiddlybase/cli run cli buildwiki -t html -p "$pwd/../../dist/plugins" -p "$pwd/plugins" -o "$pwd/../../dist/tiddlybase_public/$dirname.html" "$pwd/$dirname"
