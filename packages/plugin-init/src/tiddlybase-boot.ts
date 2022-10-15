37e513bca56a38033891960db6220d308af4eb5a
Peter Neumark <neumark.peter@gmail.com>
2022-10-15 13:08:57 +0200

    Load tiddlybase/init plugin even for non-sandboxed iframe scenarios

--- packages/plugin-init/src/tiddlybase-boot.ts
+++ packages/plugin-init/src/tiddlybase-boot.ts
@@ -11,14 +11,40 @@ import { PatchedModules } from "./patched-modules";
   // from: https://stackoverflow.com/a/37178303
   const runningIniOSChrome = () => /CriOS/i.test(navigator.userAgent) && /iphone|ipod|ipad/i.test(navigator.userAgent);
 
+  const tiddlerChangeHandler = (wikiChange: $tw.WikiChange) => {
+    // for each changed tiddler, invalidate module exports
+    const toInvalidate = new Set<string>();
+    for (let tiddler of Object.keys(wikiChange)) {
+      toInvalidate.add(tiddler);
+      for (let moduleName of ($tw.modules as PatchedModules).getAllModulesRequiring(tiddler)) {
+        toInvalidate.add(moduleName);
+      }
+    }
+    for (let m of toInvalidate) {
+      ($tw.modules as PatchedModules).invalidateModuleExports(m);
+    }
+  };
+
+  const bootTiddlyWiki = () => {
+
+    $tw.modules = new PatchedModules($tw.modules.titles, $tw.modules.types);
+    $tw.boot.boot();
+    // use unshift() instead of addEventListener beecause the invalidation
+    // needs to happen before rerendering of the tiddlers which is initiated
+    // by another "change" event listener registered earlier in the boot
+    // process
+    ($tw.wiki as any).eventListeners["change"].unshift(tiddlerChangeHandler);
+    if (runningIniOSChrome()) {
+      window.onerror = null;
+    }
+  }
+
   /**
    * The @tiddlybase/init plugin should only be part of wikis which are loaded
    * as iframes by Tiddlybase. If the current window isn't an iframe, the plugin
    * should exit.
    */
-  if (window === window.parent) {
-    return;
-  }
+  const inSandboxedIframe = window !== window.parent;
 
   // Prior to even loading preboot.js, ensure `supressBoot`
   // and other necessary variables are set.
@@ -30,71 +56,53 @@ import { PatchedModules } from "./patched-modules";
 
   window.addEventListener('load', async () => {
 
-    const rpc = makeRPC();
-    const topLevelClient = apiClient<TopLevelAPIForSandboxedWiki>(rpc, window.parent)
-    const def = apiDefiner<SandboxedWikiAPIForTopLevel>(rpc);
-    def('testParentChild', async (message: string) => {
-      console.log(message);
-    });
-    const topLevelResponse = await topLevelClient('childIframeReady', []);
-    const {
-      user,
-      launchConfig: { wikiNames, settings },
-      isLocal,
-      storageConfig,
-      parentLocation
-    } = topLevelResponse;
-    console.log('child iframe received user info', topLevelResponse);
-    window.$tw.tiddlybase = {
-      topLevelClient,
-      isLocal,
-      parentLocation,
-      user,
-      storageConfig
-    };
-    try {
-      const tiddlers: Array<$tw.TiddlerFields> =
-        (await Promise.all(
-          wikiNames.map(
-            wikiName => loadWikiTiddlers(
-              topLevelClient,
-              storageConfig,
-              wikiName,
-              isLocal)))).flat();
-      tiddlers.push(createWikiInfoConfig(settings))
-      console.log("tiddlers", tiddlers);
-      $tw.preloadTiddlerArray(tiddlers);
-      $tw.modules = new PatchedModules($tw.modules.titles, $tw.modules.types);
-      $tw.boot.boot();
-      // use unshift() instead of addEventListener beecause the invalidation
-      // needs to happen before rerendering of the tiddlers which is initiated
-      // by another "change" event listener registered earlier in the boot
-      // process
-      ($tw.wiki as any).eventListeners["change"].unshift(
-        (wikiChange: $tw.WikiChange) => {
-          // for each changed tiddler, invalidate module exports
-          const toInvalidate = new Set<string>();
-          for (let tiddler of Object.keys(wikiChange)) {
-            toInvalidate.add(tiddler);
-            for (let moduleName of ($tw.modules as PatchedModules).getAllModulesRequiring(tiddler)) {
-              toInvalidate.add(moduleName);
-            }
-          }
-          for (let m of toInvalidate) {
-            ($tw.modules as PatchedModules).invalidateModuleExports(m);
-          }
-        });
-      if (runningIniOSChrome()) {
-        window.onerror = null;
+    if (inSandboxedIframe) {
+      const rpc = makeRPC();
+      const topLevelClient = apiClient<TopLevelAPIForSandboxedWiki>(rpc, window.parent)
+      const def = apiDefiner<SandboxedWikiAPIForTopLevel>(rpc);
+      def('testParentChild', async (message: string) => {
+        console.log(message);
+      });
+      const topLevelResponse = await topLevelClient('childIframeReady', []);
+      const {
+        user,
+        launchConfig: { wikiNames, settings },
+        isLocal,
+        storageConfig,
+        parentLocation
+      } = topLevelResponse;
+      console.log('child iframe received user info', topLevelResponse);
+      window.$tw.tiddlybase = {
+        topLevelClient,
+        isLocal,
+        parentLocation,
+        user,
+        storageConfig
+      };
+      try {
+        const tiddlers: Array<$tw.TiddlerFields> =
+          (await Promise.all(
+            wikiNames.map(
+              wikiName => loadWikiTiddlers(
+                topLevelClient,
+                storageConfig,
+                wikiName,
+                isLocal)))).flat();
+        tiddlers.push(createWikiInfoConfig(settings))
+        $tw.preloadTiddlerArray(tiddlers);
+        bootTiddlyWiki();
+      } catch (e) {
+        console.dir(e);
+        if (typeof e === 'object') {
+          await topLevelClient('loadError', [(e as any)?.message]);
+        } else {
+          await topLevelClient('loadError', [String(e)]);
+        }
       }
-    } catch (e) {
-      console.dir(e);
-      if (typeof e === 'object') {
-        await topLevelClient('loadError', [(e as any)?.message]);
-      } else {
-        await topLevelClient('loadError', [String(e)]);
-      }
+    } else { // !inSandboxedIframe
+      bootTiddlyWiki();
     }
+
   });
 
 })()
