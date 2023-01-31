3f483bcffcbb6041f233438356e7e2bdcf07a0d2
Peter Neumark <neumark.peter@gmail.com>
2023-01-31 07:30:00 +0100

    remove unused packages/plugin-mdx/src/mdx-client/testrenderer.ts

--- packages/plugin-mdx/src/mdx-client/testrenderer.ts
+++
@@ -1,4 +0,0 @@
-import TestRenderer from 'react-test-renderer';
-
-// test renderer docs: https://reactjs.org/docs/test-renderer.html
-export const renderToAST = (component: any) => TestRenderer.create(component).toJSON();
