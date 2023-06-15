3d7f91b1e7f42db397e49f114d25a0374f09c31e
Peter Neumark <neumark.peter@gmail.com>
2023-06-15 11:40:54 +0200

    fix wiki html generation script to work with new CLI arguments

--- packages/wiki-builder/build-one.sh
+++ packages/wiki-builder/build-one.sh
@@ -3,4 +3,4 @@
 dirname=$(echo "$1" | cut -d / -f 2-)
 # yarn workspace run changes the current directory, so $pwd needs to be saved beforehand and all paths must be absolute in command line arguments
 pwd="$(pwd)"
-yarn workspace @tiddlybase/cli run cli buildwiki -t html -p "$pwd/../../dist/plugins" -p "$pwd/plugins" -o "$pwd/../../dist/tiddlybase_public/$dirname.html" "$pwd/$dirname"
+yarn workspace @tiddlybase/cli run cli buildwiki -t html -p "$pwd/../../dist/plugins" -p "$pwd/plugins" -o "$pwd/../../dist/tiddlybase_public/$dirname.html" --wiki-info-filename "$pwd/$dirname/tiddlywiki.info"
