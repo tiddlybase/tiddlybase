8bcc64bddd4afb9cf5070795cc2bbb7fd71b2b60
Peter Neumark <neumark.peter@gmail.com>
2023-06-24 08:39:38 +0200

    Add yarn release command

---
+++ scripts/publish_tiddlybase_public.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd "$DIR/.."
+jq -s '.[1] + {version: .[0].version}' "package.json" "tiddlybase-public-package.json" > "dist/package.json"
+cd "dist"
+npm publish --access public
+rm package.json
+popd
