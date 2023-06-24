1cfafbc0651f22ab41770b73642413dae1d4087e
Peter Neumark <neumark.peter@gmail.com>
2023-06-24 08:54:12 +0200

    move tiddlybase-public-package.json

--- scripts/publish_tiddlybase_public.sh
+++ scripts/publish_tiddlybase_public.sh
@@ -2,7 +2,7 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd "$DIR/.."
-jq -s '.[1] + {version: .[0].version}' "package.json" "tiddlybase-public-package.json" > "dist/package.json"
+jq -s '.[1] + {version: .[0].version}' "package.json" "npm-package-json/tiddlybase-public-package.json" > "dist/package.json"
 cd "dist"
 npm publish --access public
 rm package.json
