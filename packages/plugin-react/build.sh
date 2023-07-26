af5b809c6c6d5377a3b6415fb8a2d7afc80a05b6
Peter Neumark <neumark.peter@gmail.com>
2023-07-26 10:56:51 +0200

    Update various dependencies

--- packages/plugin-react/build.sh
+++ packages/plugin-react/build.sh
@@ -5,11 +5,11 @@ filesDir="$pluginDir/files"
 mkdir -p "$filesDir"
 destination="$filesDir/react.js"
 if [ ! -f "$destination" ]; then
-  curl -o "$destination" 'https://unpkg.com/react@18.0.0/umd/react.production.min.js'
+  curl -o "$destination" 'https://unpkg.com/react@18.2.0/umd/react.production.min.js'
 fi
 destination="$filesDir/react-dom.js"
 if [ ! -f "$destination" ]; then
-  curl -o "$destination" 'https://unpkg.com/react-dom@18.0.0/umd/react-dom.production.min.js'
+  curl -o "$destination" 'https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js'
 fi
 cp "../../node_modules/react/cjs/react-jsx-runtime.production.min.js" "$filesDir/react-jsx-runtime.js"
 yarn webpack
