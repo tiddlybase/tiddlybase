1de6824cb61e157f6587a2e5ee3d4c7e43ca2006
Peter Neumark <neumark.peter@gmail.com>
2023-08-18 09:18:27 +0200

    remove packages/shared dependency on lodash

--- packages/shared/src/permissions.ts
+++ packages/shared/src/permissions.ts
@@ -1,15 +1,14 @@
 import { ADMIN_INSTANCE_NAME } from "./constants";
 import type { PermissionedDataSource, InstanceSpec, UserId } from "./instance-spec-schema"
-import {merge} from 'lodash';
 
 export const instanceSpecPath = (instanceName:string) => `tiddlybase-instances/${ADMIN_INSTANCE_NAME}/collections/instances/tiddlers/${instanceName}`
 
-export const addInstancePermissions = (
+export const makeInstancePermissionsUpdate = (
   instanceSpec:InstanceSpec,
   resourceType: PermissionedDataSource,
   userId: UserId,
   collectionName: string,
-  role:number):InstanceSpec => merge(instanceSpec, {
+  role:number):InstanceSpec => ({
     permissions: {
       [userId]: {
         [resourceType]: {
